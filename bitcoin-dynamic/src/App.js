5feae4692f8dc76d29868da642f82e86a76a003f
ilya shusterman <ilyashusterman@outlook.com>
2017-06-17 15:22:32 +0300

    remove unused files added header and page base

--- bitcoin-dynamic/src/App.js
+++ bitcoin-dynamic/src/App.js
@@ -1,32 +1,60 @@
 import React, { Component } from 'react';
 import './App.css';
+import {LARGE, SMALL} from 'material-ui/utils/withWidth';
 import {MuiThemeProvider} from 'material-ui';
-import BottomNavigationBar from './components/BottomNavigationBar';
 import AIDrawer from './components/AIDrawer';
-import HeaderToolBar from './components/HeaderToolBar';
+import HeaderToolBar from './components/Header';
+import PageBase from  './components/PageBase';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin();
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            navDrawerOpen: false
+        };
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (this.props.width !== nextProps.width) {
+            this.setState({navDrawerOpen: nextProps.width === LARGE});
+        }
+    }
+
+    handleChangeRequestNavDrawer() {
+        this.setState({
+            navDrawerOpen: !this.state.navDrawerOpen
+        });
+    }
+
   render() {
+      let { navDrawerOpen } = this.state;
+      const paddingLeftDrawerOpen = 236;
+
+      const styles = {
+          header: {
+              paddingLeft: navDrawerOpen ? paddingLeftDrawerOpen : 0
+          },
+          container: {
+              margin: '80px 20px 20px 15px',
+              paddingLeft: navDrawerOpen && this.props.width !== SMALL ? paddingLeftDrawerOpen : 0
+          }
+      };
     return (
     <MuiThemeProvider>
-    <div>
+
+        <div>
       <div>
-        <div className='App-header'>
-          <h2>Welcome to Bitcoin</h2>
-        </div>
+        <HeaderToolBar styles={styles.header}
+                handleChangeRequestNavDrawer={this.handleChangeRequestNavDrawer.bind(this)}/>
       </div>
-      <div>
-        <HeaderToolBar />
-      </div>
-      <div  className='App-Bar'>
+      <PageBase title="Welcome to Bitualization">
         <AIDrawer/>
-      </div>
-        <BottomNavigationBar />
-    </div>
+      </PageBase>
+            </div>
     </MuiThemeProvider>
     );
   }
---
+++ bitcoin-dynamic/src/styles.js
@@ -0,0 +1,28 @@
+/**
+ * Created by ilya on 6/17/17.
+ */
+import {typography} from 'material-ui/styles';
+import {grey600} from 'material-ui/styles/colors';
+
+const styles = {
+    navigation: {
+        fontSize: 15,
+        fontWeight: typography.fontWeightLight,
+        color: grey600,
+        paddingBottom: 15,
+        display: 'block'
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: typography.fontWeightLight,
+        marginBottom: 20
+    },
+    paper: {
+        padding: 30
+    },
+    clear: {
+        clear: 'both'
+    }
+};
+
+export default styles;
